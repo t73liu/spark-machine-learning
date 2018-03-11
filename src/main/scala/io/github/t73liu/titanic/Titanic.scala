710798ef145de532b9bf0d8660e3ad0c9f76561f
t73liu <t73liu@gmail.com>
2018-03-11 17:13:22 -0400

    Titanic decision tree pipeline

--- src/main/scala/io/github/t73liu/titanic/Titanic.scala
+++ src/main/scala/io/github/t73liu/titanic/Titanic.scala
@@ -1,9 +1,13 @@
 package io.github.t73liu.titanic
 
 import org.apache.spark.ml.classification.DecisionTreeClassifier
+import org.apache.spark.ml.evaluation.BinaryClassificationEvaluator
 import org.apache.spark.ml.feature.VectorAssembler
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.ml.tuning.{CrossValidator, ParamGridBuilder}
+import org.apache.spark.ml.{Pipeline, PipelineModel}
 import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types.IntegerType
+import org.apache.spark.sql.{SaveMode, SparkSession}
 
 import scala.util.Random
 
@@ -26,7 +30,7 @@ object Titanic extends App {
   originalDF.cache()
 
   // Determined the fill values used
-  println("Averages to use when defaulting null")
+  println("Averages to use when defaulting invalid values")
   originalDF.na.drop(Array("Sex", "Age", "Pclass", "Fare"))
     .groupBy("Sex")
     .agg(count("Sex"), avg("Age"), avg("Pclass"), avg("Fare"))
@@ -40,32 +44,87 @@ object Titanic extends App {
       "Pclass" -> 2,
       "Fare" -> 27.27
     ))
-    .drop("Name", "Embarked", "Ticket")
+    .drop("Name", "Embarked", "Ticket", "PassengerId")
     .withColumn("Cabin", booleanToInt($"Cabin".isNotNull))
     .withColumn("Sex", booleanToInt($"Sex".eqNullSafe("male")))
 
-  // 70% train, 15% validation, 15% test
+  // 15% used for testing
   val Array(trainDF, testDF) = cleanedDF.randomSplit(Array(0.85, 0.15))
   trainDF.cache()
   testDF.cache()
   originalDF.unpersist()
 
   val inputColumns = trainDF.columns.filter(col => col != "Survived")
-  println("Features Selected: " + inputColumns.mkString(", "))
-  val features = new VectorAssembler().setInputCols(inputColumns).setOutputCol("features")
-  val numericalDF = features.transform(trainDF)
+  println(s"Features Selected: ${inputColumns.mkString(", ")}")
 
-  val classifier = new DecisionTreeClassifier()
+  // Transformer: applies a transformation onto a DataFrame
+  val featuresTransformer = new VectorAssembler().setInputCols(inputColumns).setOutputCol("features")
+
+  // Estimator: algorithm with can be fit onto a DataFrame to produce a Transformer
+  val decisionTreeEstimator = new DecisionTreeClassifier()
     .setSeed(Random.nextLong())
     .setLabelCol("Survived")
     .setFeaturesCol("features")
     .setPredictionCol("prediction")
 
-  val model = classifier.fit(numericalDF)
+  // Pipeline acts as an estimator and is made up of a series of Estimator/Transformer stages
+  val pipeline = new Pipeline().setStages(Array(featuresTransformer, decisionTreeEstimator))
 
-  println("Feature importance in prediction")
-  model.featureImportances.toArray.zip(inputColumns).sorted.reverse.foreach(println)
+  // Estimator parameter combinations to train
+  val estimatorParameters = new ParamGridBuilder()
+    .addGrid(decisionTreeEstimator.impurity, Seq("entropy", "gini"))
+    .addGrid(decisionTreeEstimator.maxDepth, Seq(10, 12, 14, 16))
+    .addGrid(decisionTreeEstimator.maxBins, Seq(25, 30))
+    .build()
+
+  // Evaluation of model using true positive rate and false positive rate
+  val binaryEvaluator = new BinaryClassificationEvaluator()
+    .setLabelCol("Survived")
+    .setMetricName("areaUnderROC")
+
+  val validator = new CrossValidator()
+    .setSeed(Random.nextLong())
+    .setEstimator(pipeline)
+    .setEvaluator(binaryEvaluator)
+    .setEstimatorParamMaps(estimatorParameters)
+    .setNumFolds(4)
+
+  val validatorModel = validator.fit(trainDF)
+
+  val bestModel = validatorModel.bestModel
+  println(s"Model Selected: ${bestModel.asInstanceOf[PipelineModel].stages.last.extractParamMap}")
+
+  val testAccuracy = binaryEvaluator.evaluate(bestModel.transform(testDF))
+  println(s"Test Accuracy: $testAccuracy")
+
+  val trainAccuracy = binaryEvaluator.evaluate(bestModel.transform(trainDF))
+  println(s"Training Accuracy: $trainAccuracy")
 
   trainDF.unpersist()
   testDF.unpersist()
+
+  // Writes output for Kaggle
+  val resultDF = spark.read
+    .option("header", "true")
+    .option("inferSchema", "true")
+    .csv("./src/test/resources/kaggle-titanic/test.csv")
+    .na
+    .fill(Map(
+      "Sex" -> "male",
+      "Age" -> 30.7,
+      "Pclass" -> 2,
+      "Fare" -> 27.27
+    ))
+    .withColumn("Cabin", booleanToInt($"Cabin".isNotNull))
+    .withColumn("Sex", booleanToInt($"Sex".eqNullSafe("male")))
+
+  bestModel.transform(resultDF)
+    .select("PassengerId", "prediction")
+    .withColumn("prediction", $"prediction".cast(IntegerType))
+    .withColumnRenamed("prediction", "Survived")
+    .coalesce(1)
+    .write
+    .mode(SaveMode.Overwrite)
+    .option("header", "true")
+    .csv("./src/test/resources/kaggle-titanic/result/")
 }
